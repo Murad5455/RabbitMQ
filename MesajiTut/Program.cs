948d3e4eb6545b17e480714194c0d05f8a8bf140
Murad5455 <118770310+Murad5455@users.noreply.github.com>
2023-12-12 23:39:58 +0400

    Add project files.

---
+++ MesajiTut/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://lmfinsjz:RdqZIcDwJ1bnTfalX_07b1D_f-Y6CDI9@rat.rmq2.cloudamqp.com/lmfinsjz");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("mesaj-quyruq",true,false,false);
+
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume("mesaj-quyruq",true,consumer);
+
+
+consumer.Received += Consumer_Received;
+Console.ReadLine();
+
+static void Consumer_Received(object sender,BasicDeliverEventArgs e)
+{
+    Console.WriteLine("Gelen mesaj :" + Encoding.UTF8.GetString(e.Body.ToArray()));
+}
---
+++ RabbitMQ/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+for(int i = 0;i<10;i++)
+{
+    MessageSend();
+}
+
+
+
+
+
+static void MessageSend()
+{
+    var factory = new ConnectionFactory();
+    factory.Uri = new Uri("amqps://lmfinsjz:RdqZIcDwJ1bnTfalX_07b1D_f-Y6CDI9@rat.rmq2.cloudamqp.com/lmfinsjz");
+
+    using var connection = factory.CreateConnection();
+
+    var channel = connection.CreateModel();
+
+    channel.QueueDeclare("mesaj-quyruq", true, false, false);
+
+    var messaj = "Deneme mesaj";
+    var Body = Encoding.UTF8.GetBytes(messaj);
+
+    channel.BasicPublish(string.Empty, "mesaj-quyruq", null, Body);
+
+    Console.WriteLine("Salam");
+    Console.ReadLine();
+}
